e417c6a31832f82e4d94f42bcd1c4d4cad61157a
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-04 09:49:23 +0200

    first commit

---
+++ src/main/java/mymenu/mymenu/MymenuApplication.java
@@ -0,0 +1,13 @@
+package mymenu.mymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymenuApplication.class, args);
+	}
+
+}
