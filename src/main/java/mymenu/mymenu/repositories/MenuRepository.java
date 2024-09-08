8dda830b7f81b26e9bb1bda0aa23979f450ac371
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-08 17:41:15 +0200

    Adding get menu

--- src/main/java/mymenu/mymenu/repositories/MenuRepository.java
+++ src/main/java/mymenu/mymenu/repositories/MenuRepository.java
@@ -1,9 +1,14 @@
 package mymenu.mymenu.repositories;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mymenu.mymenu.models.Menu;
 
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+
+    public List<Menu> findByDateBetweenOrderByDate(Date dateStart, Date dateEnd);
     
 }
